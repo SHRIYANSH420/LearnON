a3c216e303b8421a8c9d94011d1cee325a01c8cf
Shriyansh <rauthanshriyansh@gmail.com>
2025-03-21 18:36:12 +0530

    Setting Page Complete

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,8 +5,14 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import * as dynamoose from "dynamoose";
+import {
+  clerkMiddleware,
+  createClerkClient,
+  requireAuth,
+} from "@clerk/express";
 
 import courseRoutes from "./routes/courseRoutes";
+import userClerkRoutes from "./routes/userClerkRoutes";
 
 
 // CONFIGURATIONS
@@ -16,6 +22,10 @@ if (!isProduction) {
   dynamoose.aws.ddb.local();
 }
 
+export const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY,
+});  
+
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -24,13 +34,16 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(clerkMiddleware());
 
-// routes
+
+// routes 
 app.get("/", (req, res) => {
   res.send("bye bye");
 });
 
-app.use("/courses", courseRoutes)
+app.use("/courses", courseRoutes);
+app.use("/users/clerk", requireAuth(), userClerkRoutes);
 
 // Server
 const port = process.env.PORT || 3000;
